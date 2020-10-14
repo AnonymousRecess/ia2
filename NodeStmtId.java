76a3b0270240116cad5308b8eb808ac9f8068822
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2020-10-14 15:54:06 -0600

    stillneedtestread

--- NodeStmtId.java
+++ NodeStmtId.java
@@ -1,4 +1,4 @@
-public class NodeStmtId extends NodeStmt { //used by write to get stored node?
+public class NodeStmtId extends NodeStmt { //todo used by write to get stored node? This is the way to 'remember' a stored value? Or do I need it retrieved by factID IF SO MIGHT NOT NEED THIS?
 
     private String id;
 
