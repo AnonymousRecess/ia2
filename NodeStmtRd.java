42c8656f1a02309c40abc95ea43b463ec4d7a331
Jeff <jeffkahn@u.boisestate.edu>
2020-10-27 19:41:44 -0600

    TestWorking

--- NodeStmtRd.java
+++ NodeStmtRd.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class NodeStmtRd extends NodeStmt {
     private String id;
-    private static Scanner scan;
+    private static Scanner scan = new Scanner(System.in);;
     public NodeStmtRd(String id) {
         this.id = id;
     }
 
     public double eval(Environment env) {
-        scan = new Scanner(System.in);
+        
         return env.put(id, scan.nextDouble()); 
 
     }
